26749ad0abaf7cfa7378a15b016fab74655ddef8
Kate Reeher <kate.reeher@Calabrio.com>
2020-01-09 15:38:10 -0600

    i love generated test stubs

---
+++ src/test/kotlin/com/madecunningly/chons/models/BookTest.kt
@@ -0,0 +1,52 @@
+package com.madecunningly.chons.models
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BookTest {
+
+    @BeforeEach
+    fun setUp() {
+        print("""
+                LOVE TO TEST THINGS LA LA LA """.trimIndent())
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun getTitle() {
+    }
+
+    @Test
+    fun getAuthor() {
+    }
+
+    @Test
+    operator fun component1() {
+    }
+
+    @Test
+    operator fun component2() {
+    }
+
+    @Test
+    fun copy() {
+    }
+
+    @Test
+    fun testToString() {
+    }
+
+    @Test
+    fun testHashCode() {
+    }
+
+    @Test
+    fun testEquals() {
+    }
+}
